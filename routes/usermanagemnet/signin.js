381d5ed8f9a4392f2b60cc8cb1a2744e4cdbcff1
sheom <mingyov0807@gmail.com>
2020-08-12 09:58:53 +0900

    signin 뼈대만 만든상태 프로시저 연결 안되어있음.

--- routes/usermanagemnet/signin.js
+++ routes/usermanagemnet/signin.js
@@ -7,13 +7,30 @@ const gethashPassword = new hash();
 const app =express();
 
 router.post('/',isLoggedIn, async (req,res, next)=>{
-   let id = req.dody.id;
-   let pass = req.body.passoword;
-   let hashPassword = gethashPassword.get_hashed_password(pass);
+    let id = req.dody.id;
+    let pass = req.body.passoword;
+    let hashPassword = gethashPassword.get_hashed_password(pass);
 
     let signinResult = await signin(id,hashPassword);
 
-   
+   if(signinResult == 0){
+       req.session.signin={
+        usn: "get usn stored procedure",
+        wnsc:"",
+        ansc:"",
+        state:""
+       }
+       res.send("로그인 완료");
+   }else if(signinResult == 1){
+       console.error("reject other error");
+       
+   }else if(signinResult == 2){
+        console.error('없는 유저');
+        
+   }else if(signinResult == 3){
+       console.error("비멀번호 틀림!");
+       
+   }
    
    
     
