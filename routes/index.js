b57aeebeccc5205d4d63cbe5dde058d4e86dbcd6
성현 <mingyov0807@gmail.com>
2020-08-12 11:57:39 +0900

    signin databse connect , test at master

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.session.signin.wnsc == 0){
+  res.render('index', { title: "Express!" });
+  }else{
+    res.render('index',{ title:req.session.signin.usn});
+  }
 });
 
 module.exports = router;
--- routes/usermanagemnet/signin.js
+++ routes/usermanagemnet/signin.js
@@ -11,27 +11,22 @@ router.post('/',isLoggedIn, async (req,res, next)=>{
     let pass = req.body.passoword;
     let hashPassword = gethashPassword.get_hashed_password(pass);
 
-    let signinResult = await signin(id,hashPassword);
-
-   if(signinResult == 0){
+   let signinResult = await signin(id,hashPassword);
+    //let signinResult = 0;
+   if(signinResult.result == 0){
        req.session.signin={
-        usn: "get usn stored procedure",
-        wnsc:"",
-        ansc:"",
-        state:""
+        usn: signinResult.usn,
+        wnsc:signinResult.wnsc,
        }
        res.send("로그인 완료");
-   }else if(signinResult == 1){
+   }else if(signinResult.result == 1){
        console.error("reject other error");
        
-   }else if(signinResult == 2){
+   }else if(signinResult.result == 2){
         console.error('없는 유저');
         
-   }else if(signinResult == 3){
+   }else if(signinResult.result == 3){
        console.error("비멀번호 틀림!");
        
-   }
-   
-   
-    
+   }    
 });
